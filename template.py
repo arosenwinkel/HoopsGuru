4391bbc7471c70d49e4d39da4693cfdfa450ac62
Aaron Rosenwinkel <aaronrosenwinkel@gmail.com>
2015-11-08 17:30:19 -0500

    Fixed template formatting, added a couple new ideas

--- template.py
+++ template.py
@@ -91,8 +91,7 @@ age_dist_dict = dict(zip(age_dist_ages, age_dist_wgts))
 
 # Define template constants and then stick them all in a dictionary for access by other .py files
 template_list = []
-temp1 = Template("temp1", [  # LeBron James
-    "SF", "PF", 95,
+temp1 = Template("temp1", "SF", "PF", 95, [  # LeBron James, athletic versatile forward
     80, 1.053, 3.125,
     60, 75, 70, 80, 60, 55, 50, 75, 85, 70,
     80, 90, 85, 90, 85, 85,
@@ -100,8 +99,7 @@ temp1 = Template("temp1", [  # LeBron James
 ])
 template_list.append(temp1)
 
-temp2 = Template("temp2", [  # Kevin Durant
-    "SF", "PF", 95,
+temp2 = Template("temp2", "SF", "PF", 95, [  # Kevin Durant, scoring forward
     82.25, 1.079, 2.918,
     80, 70, 65, 80, 70, 45, 40, 65, 70, 75,
     75, 75, 65, 80, 80, 75,
@@ -109,8 +107,7 @@ temp2 = Template("temp2", [  # Kevin Durant
 ])
 template_list.append(temp2)
 
-temp3 = Template("temp3", [  # Russell Westbrook
-    "PG", "SG", 85,
+temp3 = Template("temp3", "PG", "SG", 85, [  # Russell Westbrook, athletic scoring guard
     75.5, 1.056, 2.543,
     75, 80, 45, 70, 75, 50, 30, 50, 70, 80,
     90, 85, 50, 95, 90, 70,
@@ -118,8 +115,7 @@ temp3 = Template("temp3", [  # Russell Westbrook
 ])
 template_list.append(temp3)
 
-temp4 = Template("temp4", [  # Steph Curry
-    "PG", "SG", 95,
+temp4 = Template("temp4", "PG", "SG", 95, [  # Steph Curry, GOAT shooter
     75.25, 1.003, 2.525,
     95, 75, 30, 75, 90, 30, 20, 55, 80, 85,
     85, 50, 20, 70, 75, 80,
@@ -127,8 +123,7 @@ temp4 = Template("temp4", [  # Steph Curry
 ])
 template_list.append(temp4)
 
-temp5 = Template("temp5", [  # DeMarcus Cousins
-    "C", "PF", 80,
+temp5 = Template("temp5", "C", "PF", 80, [  # DeMarcus Cousins, post scorer
     82.75, 1.085, 3.529,
     50, 40, 80, 85, 30, 75, 80, 40, 50, 70,
     60, 50, 80, 50, 50, 60,
@@ -136,26 +131,50 @@ temp5 = Template("temp5", [  # DeMarcus Cousins
 ])
 template_list.append(temp5)
 
-temp6 = Template("temp6", [  # Marc Gasol
-    "C", "None", 80,
+temp6 = Template("temp6", "C", "None", 80, [  # Marc Gasol, versatile center
     85, 1.056, 3.118,
     50, 35, 70, 80, 20, 55, 65, 70, 65, 70,
     35, 35, 90, 40, 40, 55,
     50, 75, 30, 80, 45, 30, 75
 ])
+template_list.append(temp6)
 
-temp7 = Template("temp7", [  # Blake Griffin
-    "PF", "None", 80,
+temp7 = Template("temp7", "PF", "None", 80, [  # Blake Griffin, athletic scorer
     82, 1.015, 3.061,
     40, 50, 60, 65, 40, 50, 60, 40, 55, 60,
     60, 80, 80, 60, 60, 60,
     35, 40, 45, 40, 50, 35, 40
 ])
+template_list.append(temp7)
 
-temp8 = Template("temp8", [  # James Harden
-    "SG", "None", 85,
+temp8 = Template("temp8", "SG", "None", 85, [  # James Harden, scoring 2 guard
     77.25, 1.071, 2.874,
     80, 90, 50, 80, 85, 60, 35, 35, 60, 85,
     80, 65, 45, 80, 75, 90,
     60, 40, 75, 40, 75, 80, 35
 ])
+template_list.append(temp8)
+
+temp9 = Template("temp9", "SG", "SF", 95, [  # Michael Jordan, GOAT 2 guard
+    78, 1.071, 2.769,
+    70, 95, 70, 95, 80, 65, 40, 80, 60, 90,
+    80, 95, 50, 80, 85, 90,
+    75, 80, 65, 85, 75, 85, 90
+])
+template_list.append(temp9)
+
+temp10 = Template("temp10", "C", "None", 85, [  # Dwight Howard, athletic rim protector
+    82.25, 1.076, 2.918,
+    25, 35, 50, 35, 20, 85, 75, 80, 30, 40,
+    75, 80, 90, 70, 65, 50,
+    35, 75, 20, 80, 30, 20, 80
+])
+template_list.append(temp10)
+
+temp11 = Template("temp11", "PF", "None", 70, [  # Serge Ibaka, athletic rim protector, shoots 3's
+    82, 1.073, 2.878,
+    60, 30, 50, 35, 30, 80, 65, 80, 35, 40,
+    70, 70, 80, 60, 60, 55,
+    35, 75, 40, 75, 35, 25, 75
+])
+template_list.append(temp11)
