424c74b6fb7763be722b2549bb4b2daeeedce189
Aaron Rosenwinkel <aaronrosenwinkel@gmail.com>
2015-12-05 19:55:37 -0500

    Cleaned up Scouting functions, added explanation at the top

--- Scouting.py
+++ Scouting.py
@@ -1,6 +1,29 @@
 # Scouting.py
 # Different tools relating to scouting Players
 
+'''
+
+When I scout a player, I should get back a report with the following:
+Some Vitals
+Overall Grade (A-F)
+Grades for {Skill, Athleticism, Intangible, Size, Durability, Potential}
+Grades for skill groups based on position
+--- Expandable to see how the player ranks in each individual attribute
+
+The CPU cares about:
+Some Vitals
+Overall Grade
+Grades (don't need to be expandable)
+
+So we should be able to request one of two things (for now)
+A "data" report (just numbers)
+A "written" report (grades + strings for writeups)
+Same class, written version just has more information tied to it 
+
+Both compiled using same equations, etc.
+
+'''
+
 from Player import Player
 
 
@@ -23,31 +46,34 @@ def scout_pro_primary(this_player):  # takes in a Player object and returns a sc
 class ScoutingReport:
     def __init__(self, this_player):
         self.this_player = this_player
+        self.overall = 0
+        self.basic_stats = {}
+        self.pos_stats = {}
+        self.printed_info = ""
 
     def print(self):
-        pass
+        print(self.printed_info)
 
 
 class ProPGReport(ScoutingReport):
-    def scout(self):
+    def scout(self, v=False):  # v = verbose, a.k.a. I want string information as well as numbers
         pass
 
-
 class ProSGReport(ScoutingReport):
-    def scout(self):
+    def scout(self, v=False):
         pass
 
 
 class ProSFReport(ScoutingReport):
-    def scout(self):
+    def scout(self, v=False):
         pass
 
 
 class ProPFReport(ScoutingReport):
-    def scout(self):
+    def scout(self, v=False):
         pass
 
 
 class ProCReport(ScoutingReport):
-    def scout(self):
+    def scout(self, v=False):
         pass
